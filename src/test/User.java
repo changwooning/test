4e5bba1d8a887068aaec821b6487df09cf67b146
sonchangwoo <thsckddn97@naver.com>
2024-09-23 16:35:02 +0900

    User create getter & constructor !

--- src/test/User.java
+++ src/test/User.java
@@ -7,5 +7,33 @@ public class User {
 	private String name;
 	private String id;
 	private String password;
+	
+	public User () {}
+	
+	public User(int code, String id, String password) {
+		this.code = code;
+		this.id = id;
+		this.password = password;
+	}
+	
+	public Cart getCart() {
+		return this.cart;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
 
 }
